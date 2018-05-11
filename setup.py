0d1f2e0b67ffb3a82dd197da095170a0a271d40c
Matthias <37593530+baumi-berlin@users.noreply.github.com>
2018-05-11 15:41:19 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='baumiTools',
+      version='0.1',
+      description='python package containing small utility functions',
+      url='http://github.com/baumi-berlin/baumiTools',
+      author='Matthias Baumann',
+      author_email='matthias.baumann@hu-berlin.de',
+      license='MIT',
+      packages=['baumiTools'],
+      zip_safe=False)
